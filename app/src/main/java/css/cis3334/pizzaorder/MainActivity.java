8e8ea7397324072a7324b303f5092c8fe929aab1
Tracy Yang <jewel7meme@gmail.com>
2017-02-10 11:37:37 -0600

    Feb. 10th Finished task 1: Work on the interface between the MainActivity and the PizzaOrder class so users can order certain pizzas. Partially finished task 2: Add a Spinner that lists different topics and allows the user to select one topping.

--- app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
+++ app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
@@ -3,10 +3,16 @@ package css.cis3334.pizzaorder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.RadioButton;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements updateViewInterface {
 
@@ -18,6 +24,8 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
     TextView txtTotal;
     TextView txtStatus;
     PizzaOrderInterface pizzaOrderSystem;
+    Spinner spinner;
+    String itemSelected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +42,46 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
         txtTotal = (TextView) findViewById(R.id.textViewTotal);
         txtStatus = (TextView) findViewById(R.id.textViewStatus);
 
+        spinner = (Spinner) findViewById(R.id.spinner);
+
+        List<String> toppings = new ArrayList<String>();
+        toppings.add("Chicken");
+        toppings.add("Vegetable");
+        toppings.add("Cheese");
+
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, toppings);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
         pizzaOrderSystem = new PizzaOrder(this);
     }
 
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        // On selecting a spinner item
+        String itemSelected = parent.getItemAtPosition(position).toString();
+
+        // Showing selected spinner item
+        Toast.makeText(parent.getContext(), "Selected: " + itemSelected, Toast.LENGTH_LONG).show();
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
     @Override
     public void updateView(String orderStatus) {
         txtStatus.setText("Order Status" + orderStatus);
     }
 
     public void onClickOrder(View view) {
-        String orderDescription = pizzaOrderSystem.OrderPizza("Pepperoni","Large", false);
+        String size = "";
+            if (rbSmall.isChecked()){size = "small";}
+        if (rbMedium.isChecked()){size = "medium";}
+        if (rbLarge.isChecked()){size = "large";}
+        String orderDescription = pizzaOrderSystem.OrderPizza(itemSelected, size, false);
         //display a pop up message for a long period of time
         Toast.makeText(getApplicationContext(), "You have ordered a "+orderDescription , Toast.LENGTH_LONG).show();
         txtTotal.setText("Total Due: " + pizzaOrderSystem.getTotalBill().toString());
