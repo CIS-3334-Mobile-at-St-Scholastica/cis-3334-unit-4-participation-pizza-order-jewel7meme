6449ddc7b2ae6cc42c2489a480636cdc3fde1757
Tracy Yang <jewel7meme@gmail.com>
2017-02-12 17:17:32 -0600

    Assignment 4: 1. Made changes to the MainActivity and PizzaOrder class. 2. Added a spinner with a hint option.

--- app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
+++ app/src/main/java/css/cis3334/pizzaorder/MainActivity.java
@@ -1,8 +1,10 @@
 package css.cis3334.pizzaorder;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
@@ -10,6 +12,11 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -24,8 +31,15 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
     TextView txtTotal;
     TextView txtStatus;
     PizzaOrderInterface pizzaOrderSystem;
+
     Spinner spinner;
-    String itemSelected;
+    String[] toppings;
+    String sTopping;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,47 +57,83 @@ public class MainActivity extends AppCompatActivity implements updateViewInterfa
         txtStatus = (TextView) findViewById(R.id.textViewStatus);
 
         spinner = (Spinner) findViewById(R.id.spinner);
+        toppings = getResources().getStringArray(R.array.toppings);
 
-        List<String> toppings = new ArrayList<String>();
+        pizzaOrderSystem = new PizzaOrder(this);
+        /*List<String> toppings = new ArrayList<String>();
         toppings.add("Chicken");
         toppings.add("Vegetable");
-        toppings.add("Cheese");
+        toppings.add("Cheese");*/
 
 
         // Creating adapter for spinner
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, toppings);
+        final ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, toppings) {
 
+            public boolean isEnabled(int position) {
+                if (position == 0)
+                    return false;
+                else
+                    return true;
+            }
+
+            public View getDropDownView(int position, View convertView, ViewGroup parent) {
+                View view = super.getDropDownView(position, convertView, parent);
+                TextView tv = (TextView) view;
+                if (position == 0)
+                    tv.setTextColor(Color.GRAY);
+                else
+                    tv.setTextColor(Color.BLACK);
+                return view;
+            }
+        };
         // Drop down layout style - list view with radio button
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         // attaching data adapter to spinner
         spinner.setAdapter(dataAdapter);
-        pizzaOrderSystem = new PizzaOrder(this);
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapter, View view, int position, long id) {
+                //On selecting a spinner item
+                sTopping = adapter.getItemAtPosition(position).toString();
+                // If user change the default selection
+                // First item is disable and it is used for hint
+                if (position > 0) {
+                    // Notify the selected item text
+                    // Showing selected spinner item
+                    Toast.makeText(getApplicationContext(), "Selected Topping: " + sTopping, Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
     }
 
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-        // On selecting a spinner item
-        String itemSelected = parent.getItemAtPosition(position).toString();
 
-        // Showing selected spinner item
-        Toast.makeText(parent.getContext(), "Selected: " + itemSelected, Toast.LENGTH_LONG).show();
-    }
-    public void onNothingSelected(AdapterView<?> arg0) {
-        // TODO Auto-generated method stub
-    }
-    @Override
-    public void updateView(String orderStatus) {
-        txtStatus.setText("Order Status" + orderStatus);
-    }
 
-    public void onClickOrder(View view) {
-        String size = "";
-            if (rbSmall.isChecked()){size = "small";}
-        if (rbMedium.isChecked()){size = "medium";}
-        if (rbLarge.isChecked()){size = "large";}
-        String orderDescription = pizzaOrderSystem.OrderPizza(itemSelected, size, false);
-        //display a pop up message for a long period of time
-        Toast.makeText(getApplicationContext(), "You have ordered a "+orderDescription , Toast.LENGTH_LONG).show();
-        txtTotal.setText("Total Due: " + pizzaOrderSystem.getTotalBill().toString());
-    }
-}
+            public void updateView(String orderStatus) {
+                txtStatus.setText("Order Status" + orderStatus);
+            }
+
+            public void onClickOrder(View view) {
+                String size = "";
+                if (rbSmall.isChecked()) {
+                    size = "small";
+                }
+                if (rbMedium.isChecked()) {
+                    size = "medium";
+                }
+                if (rbLarge.isChecked()) {
+                    size = "large";
+                }
+                String orderDescription = pizzaOrderSystem.OrderPizza(sTopping, size, false);
+                //display a pop up message for a long period of time
+                Toast.makeText(getApplicationContext(), "You have ordered a " + orderDescription, Toast.LENGTH_LONG).show();
+                txtTotal.setText("Total Due: " + pizzaOrderSystem.getTotalBill().toString());
+            }
+        }
